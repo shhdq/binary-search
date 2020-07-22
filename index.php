4f690276e617fb6b3c493aee3d31e1dda67dcb2d
Madars B <balodis.madars@gmail.com>
2020-07-22 12:41:07 +0300

    start working on search

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+function binarySearch(Array $arr, $x) { 
+    // check for empty array 
+    if (count($arr) === 0) return false; 
+    $low = 0; 
+    $high = count($arr) - 1; 
+      
+}
+?>
