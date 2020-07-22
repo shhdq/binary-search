0c4b67957f32dc060e159b755fafcfefd0cc0f14
Madars B <balodis.madars@gmail.com>
2020-07-22 12:46:00 +0300

    check for position

--- index.php
+++ index.php
@@ -6,5 +6,23 @@ function binarySearch(Array $arr, $x) {
     $low = 0; 
     $high = count($arr) - 1; 
       
+    while ($low <= $high) { 
+          
+        // compute middle index 
+        $mid = floor(($low + $high) / 2); 
+   
+        // element found at mid 
+        if($arr[$mid] == $x) { 
+            return true; 
+        } 
+  
+        if ($x < $arr[$mid]) { 
+            // search the left side of the array 
+            $high = $mid -1; 
+        } 
+        else { 
+            // search the right side of the array 
+            $low = $mid + 1; 
+        } 
+    } 
 }
-?>
